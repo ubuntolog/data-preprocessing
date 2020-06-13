44b22c540f195fd051614952af816f12d90044b7
Alexandr Chernov <kstchernov@gmail.com>
2020-06-13 12:25:18 +0200

    Added an initial set of scripts

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # data-preprocessing
 My personal collection of scripts to clean up raw data
+
+## duck.py
+A Python script to search for images on https://duckduckgo.com/
+
+## separate_train_from_test.py
+A script that creates training and test datasets from a single folder with image files
---
+++ duck.py
@@ -0,0 +1,154 @@
+import requests;
+import re;
+import json;
+import time;
+import logging;
+import time
+import os
+from datetime import datetime
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+DATA_LOCATION = "/Users/minialex/Storage/pics/"
+MAX_RESULT = 1000
+
+def search(keywords, class_name, max_results=None):
+    os.mkdir(DATA_LOCATION + class_name)
+    url = 'https://duckduckgo.com/'
+    params = {
+    	'q': keywords
+    }
+
+    logger.debug("Hitting DuckDuckGo for Token")
+
+    #   First make a request to above URL, and parse out the 'vqd'
+    #   This is a special token, which should be used in the subsequent request
+    res = requests.post(url, data=params)
+    searchObj = re.search(r'vqd=([\d-]+)\&', res.text, re.M|re.I)
+
+    if not searchObj:
+        logger.error("Token Parsing Failed !")
+        return -1
+
+    logger.debug("Obtained Token")
+
+    headers = {
+        'authority': 'duckduckgo.com',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'sec-fetch-dest': 'empty',
+        'x-requested-with': 'XMLHttpRequest',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'referer': 'https://duckduckgo.com/',
+        'accept-language': 'en-US,en;q=0.9',
+    }
+
+    params = (
+        ('l', 'us-en'),
+        ('o', 'json'),
+        ('q', keywords),
+        ('vqd', searchObj.group(1)),
+        ('f', ',,,'),
+        ('p', '1'),
+        ('v7exp', 'a'),
+    )
+
+    requestUrl = url + "i.js"
+
+    logger.debug("Hitting Url : %s", requestUrl)
+    counter = 0
+    while True:
+        while True:
+            try:               
+                res = requests.get(requestUrl, headers=headers, params=params, timeout=5)
+                data = json.loads(res.text)
+                
+                break
+            except ValueError as e:
+                logger.debug("Hitting Url Failure - Sleep and Retry: %s", requestUrl)
+                time.sleep(5)
+                continue
+
+        logger.debug("Hitting Url Success : %s", requestUrl)
+        # print(len(data["results"]))
+        # # date_time = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
+        # print("date and time:",date_time)	
+        printJson(data["results"], class_name, counter)
+        counter = counter + len(data["results"])
+       
+
+        if "next" not in data:
+            logger.debug("No Next Page - Exiting")
+            return
+
+        requestUrl = url + data["next"]
+
+def printJson(objs, subfolder, counter):
+    for obj in objs:
+        counter = counter + 1
+        print ("Width {0}, Height {1}".format(obj["width"], obj["height"]))
+        print ("Thumbnail {0}".format(obj["thumbnail"]))
+        print ("Url {0}".format(obj["url"]))
+        print ("Title {0}".format(obj["title"].encode('utf-8')))
+        print ("Image {0}".format(obj["image"]))
+        print (counter)
+        print ("__________")
+
+        file_name = obj["image"].split(".")
+        if len(file_name)>0:     
+            extension = file_name[-1]    
+            extra_chars = file_name[-1].split("?", 1)  
+            if (len(extra_chars)>0):
+                extension = file_name[-1].split("?", 1)[0]
+
+            saveImage(obj, str(counter), extension, subfolder)
+            # time.sleep(0.1)
+
+        if (int(counter)>MAX_RESULT):
+            exit(0)
+
+
+
+
+def saveImage(obj, out_name, out_extension, subfolder):    
+    img_link = obj['image']
+
+    try:
+        img_data = requests.get(img_link, timeout=5).content
+        
+        filename = DATA_LOCATION + subfolder + "/" + out_name + "." + out_extension
+        try:
+            with open(filename, 'wb+') as f:
+                f.write(img_data)
+        except:
+            print("Cannot save an image")
+
+        print("File " + filename + " successfully downloaded")
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        print(e)
+
+   
+
+  
+   
+        
+
+if __name__ == '__main__':
+# while True:
+    # keyword = input('Enter the search keyword : ')
+    # # print(keyword)
+    # search(keyword)
+
+    food = [ 
+        ["penne", "penne"],
+        ["shashlik", "shashlik"],
+        ["heineken bottle", "heineken_bottle"],
+        ["beks bottle", "beks_bottle"],
+       
+    ]
+
+    for f in food:
+        print(f[0], f[1])    
+        search(f[0], f[1], 100)
